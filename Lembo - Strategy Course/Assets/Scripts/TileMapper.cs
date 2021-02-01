751dc2a40a4b8c4475a5aa1eaf4bf5c1d23845a8
NIDHOGGR\nathan <nlembo@runbox.com>
2021-02-01 11:30:45 -0500

    Section 2 - Units 4 to 6
    Uploaded the art assets we will be using for the course. Then we laid out the sprites and tiles into the project. From there, we created a tile mapper script that allows us to interact with our tile prefabs. So far we have randomized the tiles graphics as well as created a mouse hover animation to play

---
+++ Lembo - Strategy Course/Assets/Scripts/TileMapper.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMapper : MonoBehaviour
+{
+
+    private SpriteRenderer RendSwapper; //This will act as the owner tile's render graphic that will be swapped
+    public Sprite[] TileSwapArray; //This will hold all our possible tile arrays
+
+    public float HoverAmount; //Will control the intensity of the mouse hoving on the tile
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Gets the Tile's render component, aka the image itself
+        RendSwapper = GetComponent<SpriteRenderer>();
+        //Generates a random tile graphic based on the elements in the TileSwap Array
+        int RandTile = Random.Range(0, TileSwapArray.Length);
+        //Sets the new render graphic to that element
+        RendSwapper.sprite = TileSwapArray[RandTile];
+    }
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+    private void OnMouseEnter()
+    {
+        transform.localScale += Vector3.one * HoverAmount; //Sizes the scale of the tile based on the hover amount to show a simple graphic 
+    }
+
+    private void OnMouseExit()
+    {
+        transform.localScale -= Vector3.one * HoverAmount; //Returnns the tile to its default height
+    }
+}
